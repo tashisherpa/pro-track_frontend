f45388c23f8491ec3ac44b670791b7182fc95428
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-07-12 13:33:46 -0400

    made linkbuttons to navigate to login and signup forms

---
+++ src/components/LoginNavbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
+
+
+const LinkButton = ({ to, children, onClick }) => (
+  <NavLink to={to} onClick={onClick}>
+    {children}
+  </NavLink>
+);
+
+function LoginNavbar() {
+  return (
+    <div>
+        <nav>
+            <LinkButton to="/login">Login</LinkButton>
+            <LinkButton to="/signup">Sign Up</LinkButton>
+        </nav>
+    </div>
+  )
+}
+
+export default LoginNavbar
