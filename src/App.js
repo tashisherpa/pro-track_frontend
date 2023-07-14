545716d73f4c5203a96055681372fc0ccb1f4fdb
Tashi <tashishyangba23@gmail.com>
2023-07-14 15:58:03 -0400

    Feature authentication, separated login and signup pages and signup adds new users to the data base with their info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import { Assignments, Dashboard, HelpRequests, Resources, Lectures, Users, Profile} from "./pages";
+import { Login, Signup } from "./components/Auth";
+import {
+  Assignments,
+  Dashboard,
+  HelpRequests,
+  Resources,
+  Lectures,
+  Users,
+  Profile,
+} from "./pages";
 import PrivateRoute from "./Utils/Auth";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<Login name="login" displayName="Log In" />}
-        />
+        <Route path="/" element={<Login name="login" displayName="Log In" />} />
         <Route
           path="/signup"
           element={<Signup name="signup" displayName="Sign Up" />}
@@ -18,14 +23,19 @@ function App() {
         {/*Protected Route:
             - has all the routes that shouldn't be access by someone who isn't a user/logged in
         */}
-       <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard/>}/> {/**tashi */}
-          <Route path="/resources" element={<Resources/>}/>{/**Sabina */}
-          <Route path="/lectures" element={<Lectures/>}/>{/**Sabina */}
-          <Route path="/assignments" element={<Assignments/>}/>{/**tashi */}
-          <Route path="/helprequests" element={<HelpRequests/>}/>{/**tashi */}
-          <Route path="/users" element={<Users/>}/> {/**WIP */}
-          <Route path="/profile" element={<Profile/>}/>{/**Sabina */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} /> {/**tashi */}
+          <Route path="/resources" element={<Resources />} />
+          {/**Sabina */}
+          <Route path="/lectures" element={<Lectures />} />
+          {/**Sabina */}
+          <Route path="/assignments" element={<Assignments />} />
+          {/**tashi */}
+          <Route path="/helprequests" element={<HelpRequests />} />
+          {/**tashi */}
+          <Route path="/users" element={<Users />} /> {/**WIP */}
+          <Route path="/profile" element={<Profile />} />
+          {/**Sabina */}
         </Route>
       </Routes>
     </Router>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,39 +1,38 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import { auth, fetchSingleUserThunk } from "../redux/users/users.action";
-import { useNavigate } from "react-router-dom";
-import LoginNavbar from "./LoginNavbar";
+import { loginAuth } from "../../redux/users/users.action";
+import { Link, useNavigate } from "react-router-dom";
 
 /**
  * COMPONENT
  */
-const AuthForm = ({ name, displayName }) => {
-    const user = useSelector((state) => state.users.singleUser);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+const Login = ({ name, displayName }) => {
+  const user = useSelector((state) => state.users.singleUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        const email = evt.target.email.value;
-        console.log("email: ", email);
-        const password = evt.target.password.value;
-        dispatch(auth(email, password));
-    
-        navigate("/dashboard");
-      };
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const email = evt.target.email.value;
+    console.log("email: ", email);
+    const password = evt.target.password.value;
+    dispatch(loginAuth(email, password));
+    navigate("/dashboard");
+  };
 
   return (
     <div>
-      <LoginNavbar />
       <div className="flex flex-col h-screen items-center justify-center">
         <h1>{name}</h1>
         <div className="w-full max-w-xs">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" name={name} onSubmit={handleSubmit}>
+          <form
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            name={name}
+            onSubmit={handleSubmit}
+          >
             <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-              >
+              <label className="block text-gray-700 text-sm font-bold mb-2">
                 Email
               </label>
               <input
@@ -58,13 +57,19 @@ const AuthForm = ({ name, displayName }) => {
                 placeholder="******************"
               />
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-center space-x-10">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 {displayName}
               </button>
+              <Link
+                to="/signup"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Sign Up
+              </Link>
             </div>
           </form>
         </div>
@@ -76,10 +81,9 @@ const AuthForm = ({ name, displayName }) => {
 /**
  * PROP TYPES
  */
-AuthForm.propTypes = {
+Login.propTypes = {
   name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
 };
 
-export const Login = AuthForm;
-export const Signup = AuthForm;
+export default Login;
