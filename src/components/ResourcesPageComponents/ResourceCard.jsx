690bea024123fdb5a25bcfe8261fff40891b2fc9
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-07-14 17:08:05 -0400

    Create resources page admin/ta view

--- src/components/ResourcesPageComponents/ResourceCard.jsx
+++ src/components/ResourcesPageComponents/ResourceCard.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
 function ResourceCard() {
+  const [isAdmin, setIsAdmin] = useState(true);
+
   return (
     <div className="max-w-sm rounded-lg overflow-hidden shadow-xl">
-        <div className="px-6 py-4">
-            <div className="font-bold text-2xl mb-2">Resource Title</div>
-            <p className="text-gray-500 text-m">
-                Resource description here 
-            </p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-            <a href="https://www.npmjs.com/package/@dhaiwat10/react-link-preview" target="blank">
-                <span className="inline-block bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Link</span>
-            </a>
-        </div>
+      <div className="px-6 py-4">
+        <div className="font-bold text-2xl mb-2">Resource Title</div>
+        <p className="text-gray-500 text-m">Resource description here</p>
+      </div>
+      <div className="flex justify-between px-6 pt-4 pb-2">
+        <a
+          href="https://www.npmjs.com/package/@dhaiwat10/react-link-preview"
+          target="blank"
+        >
+          <button className=" bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+            Link
+          </button>
+        </a>
+        {
+          /*Only visible to TA/Admins */
+          isAdmin ? (
+              <button className=" bg-gray-200 dark:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                Edit
+              </button>
+          ) : null
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default ResourceCard
+export default ResourceCard;
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ResourceCard from '../components/ResourcesPageComponents/ResourceCard'
-import SideNavBar from '../components/SideNavBar'
+import React from "react";
+import AddResourceBtn from "../components/ResourcesPageComponents/AddResourceBtn";
+import ResourceCard from "../components/ResourcesPageComponents/ResourceCard";
+import SideNavBar from "../components/SideNavBar";
 
 function Resources() {
   return (
     <div>
-        <SideNavBar />
-        <div className="p-4 sm:ml-64">
-            <ResourceCard />
-        </div>
+      <SideNavBar />
+      <div className="p-4 sm:ml-64">
+        <h1 className="text-2xl font-bold mb-4">Resources</h1>
+        <AddResourceBtn />
+        <ResourceCard />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Resources
+export default Resources;
