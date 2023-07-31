8e04aacbff1b236ece2502a0ffccbace2704392e
rahimaaa <rakhimakhabib@gmail.com>
2023-07-30 22:19:12 -0400

    reomve hard coded image link

--- src/components/UserPageComponents/SingleUserCard.jsx
+++ src/components/UserPageComponents/SingleUserCard.jsx
@@ -21,7 +21,7 @@ function SingleUserCard({ user }) {
           <div className="flex items-center space-x-4">
             <img
               className="w-24 h-24 rounded-full"
-              src={img_link}
+              src={user.imageUrl}
               alt="Rounded avatar"
             />
             <div className="font-medium text-black">
@@ -57,36 +57,4 @@ function SingleUserCard({ user }) {
 }
 
 export default SingleUserCard;
-/*
- <div className="max-w-xs rounded w-40  h-50 bg-white  shadow-lg mt-4 mr-4">
-      <div className="flex justify-center mt-4">
-      <img className="w-24 h-24 rounded-full " src={img_link} alt="user_img" />
-      <div className="flex-col px-4 py-4">
-        <div className="px-6 py-4">
-          
-          <div className="text-lg text-left mb-2">
-            {user.userType}
-          </div>
-          <div className="text-lg text-left mb-2">
-            Email: {user.email}
-          </div>
-          <div className="text-lg text-left mb-2">
-            Cohort Year: {user.cohort_year}
-          </div>
-        </div>
-        <div>
-          <Link to={`/users/editUser/${user.id}`}>
-            <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2">
-              Edit
-            </button>
-          </Link>
-          <button
-            className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2"
-            onClick={handleDelete}
-          >
-            Remove
-          </button>
-        </div></div>
-      </div></div>
 
-*/
